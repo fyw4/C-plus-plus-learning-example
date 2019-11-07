c23edac390f15df604a1efe6f3cb343958f59f9a
麒麒川 <wangqingchuan92@126.com>
2019-11-07 21:44:34 +0800

    完成HelloWorld

---
+++ HelloWorld/HelloWord/HelloWord/HelloWord.cpp
@@ -0,0 +1,11 @@
+#include"stdafx.h"
+#include<iostream>
+#include<windows.h>
+
+int main()
+{
+	std::cout<< "HelloWord!"<<std::endl;
+	system("pause");
+	return 0;
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # C-plus-plus-learning-example
 自学习C++的例子
+2019.11.7：
+完成HelloWorld
