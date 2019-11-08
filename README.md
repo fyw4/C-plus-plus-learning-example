573959baa5b9609cf501b57a09aeb101b0b254c0
麒麒川 <wangqingchuan92@126.com>
2019-11-08 15:10:02 +0800

    完成游戏人物属性输出项目

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 
 1.2019.11.7：
 	完成HelloWorld，使用VS2010。
+	
 2.2019.11.8：
-	完成Student message，使用VS2010.
+	1.完成Student message，使用VS2010。
+	2.完成游戏人物属性输出，使用vs2010。
---
+++ 输出游戏人物属性/输出游戏人物属性/输出游戏人物属性.cpp
@@ -0,0 +1,20 @@
+// 输出游戏人物属性.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include<iostream>
+#include<windows.h>
+using namespace std;
+int main()
+{
+	//cout：输出对象
+	cout<<"英雄名称：寒冰射手·艾希\n";
+
+	cout<<"伤害：56\t\t攻击距离：600\n"
+		<<"护甲：15.5（+3.4）\t魔抗：30（+0.0）\n"
+		<<"生命值：395（+79）\t生命回复：0.9（+0.11）"<<endl;
+
+	system("pause");
+	return 0;
+}
+
