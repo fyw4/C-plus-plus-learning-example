1e8e024e8ffee640cb4baf090e1b3cc2ca77871c
麒麒川 <wangqingchuan92@126.com>
2019-11-08 13:30:49 +0800

    完成Student message

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # C-plus-plus-learning-example
 自学习C++的例子
-2019.11.7：
-完成HelloWorld
+
+1.2019.11.7：
+	完成HelloWorld，使用VS2010。
+2.2019.11.8：
+	完成Student message，使用VS2010.
---
+++ Students mesaage/Students mesaage/Students mesaage.cpp
@@ -0,0 +1,13 @@
+// Students mesaage.cpp : 定义控制台应用程序的入口点。
+//
+#include<iostream>
+#include<windows.h>
+#include"student.h"
+int main()
+{
+	Student stud;
+	stud.display();
+	system("pause");
+	return 0;
+}
+
