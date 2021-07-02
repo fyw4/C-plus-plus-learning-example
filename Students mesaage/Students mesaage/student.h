2d3dde849df017f571f14bbc485f571390c97e6c
Wang Qingchuan <wangqingchuan92@126.com>
2021-07-02 10:32:52 +0800

    1.第七章例题更新; 2.删除无用文件及代码。

--- Students mesaage/Students mesaage/student.h
+++
@@ -1,9 +0,0 @@
-class Student
-{
-public:
-	void display();
-private:
-	int num;
-	char name[20];
-	char sex;
-};
---
+++ 第7章 继承与派生/7_10/date.h
@@ -0,0 +1,26 @@
+#ifndef __DATE_H__
+#define __DATE_H__
+
+class Date{
+private:
+	int year;
+	int month;
+	int day;
+	int totalDays;
+public:
+	Date(int year, int month, int day);
+	int getYear() const{return year;}
+	int getMonth() const{return month;}
+	int getDay() const{return day;}
+	int getMaxDay() const;//获得当月有多少天
+	bool isLeapYear() const{
+		return year%4 == 0&& year%100 != 0 || year%400 == 0;
+	}
+	
+	void show() const;
+	//计算两个日期之间差多少天
+	int distance(const Date &date) const{
+		return totalDays - date.totalDays;
+	}
+};
+#endif
