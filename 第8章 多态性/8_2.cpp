647aa799686748a49d5d873985a4a9f79fea75a9
Wang Qingchuan <wangqingchuan92@126.com>
2021-07-06 16:19:22 +0800

    第八章例题更新

---
+++ 第8章 多态性/8_2.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+class Clock{	
+public:
+	Clock(int hour = 0, int minute = 0, int second = 0);
+	void showTime() const;
+	Clock & operator++();	//前置单目运算符重载
+	Clock operator++(int);	//后置单目运算符重载
+private:
+	int hour, minute, second;
+};
+
+Clock::Clock(int hour, int minute, int second){
+	if(0 <= hour && hour < 24 && 0 <= minute && minute < 60 
+		&& 0 <= second && second < 60){
+		this->hour = hour;
+		this->minute = minute;
+		this->second = second;
+	}else
+		cout << "Time error!" << endl;
+}
+
+void Clock::showTime() const{
+	cout << hour << ":" << minute << ":" << second << endl;
+}
+
+Clock &Clock::operator++(){	//前置单目运算符重载函数
+	second++;
+	if(second >= 60){
+		second -= 60;
+		minute++;
+		if(minute >= 60){
+			minute -= 60;
+			hour = (hour + 1) % 24;
+		}
+	}
+	return *this;
+}
+
+Clock Clock::operator++(int){	//后置单目运算符重载函数
+	Clock old = *this;
+	++(*this);
+	return old;
+}
+
+int main(){
+	Clock myClock(23, 59, 59);
+	cout << "First time output" << endl;
+	myClock.showTime();
+	cout << "Show myClock++:";
+	(myClock++).showTime();
+	cout << "Show++myClock:";
+	(++myClock).showTime();
+	
+	return 0;
+}
